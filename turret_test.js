82aa821053168c17e2d78d3476e0ccb7310543d0
sdunham <dunham.scott@gmail.com>
2012-10-26 15:13:43 -0700

    Update turret_test.js
    Adding comments

--- turret_test.js
+++ turret_test.js
@@ -15,11 +15,14 @@ var merge = function(a, b) {
 	}
 	return a;
 }
+
+//Define gameState.json default format
 gameState = new Object()
 gameState.turret = new Object()
 gameState.turret.ID = "1"
 gameState.turret.position = "5,4,3";
 
+//Create server and listen for GET/POST requests
 http.createServer(function(req, res) {
 	if (req.method == 'POST') {
 		var body = '';
@@ -31,13 +34,13 @@ http.createServer(function(req, res) {
 			res.writeHead(200, { });
 			//This should be modified to return 200 on success and something else on failure.
 			res.end();
-			merge(gameState, JSON.parse(body));
+			merge(gameState, JSON.parse(body)); //Merge POSTed data with current gamestate.json
 		});
 	} else if (req.method == 'GET') {
 		res.writeHead(200, {
 			'Content-Type' : 'application/json'
 		});
-		res.end(JSON.stringify(gameState));
+		res.end(JSON.stringify(gameState)); //return gameState.json
 	}
 
-}).listen(4242, "0.0.0.0");
+}).listen(4242, "0.0.0.0"); //Listen for POST/GET requests on port 4242
