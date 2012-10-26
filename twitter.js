c4d7f231bcfc867ec117d509184fac3a9ac9d6a8
sdunham <dunham.scott@gmail.com>
2012-10-26 15:10:53 -0700

    Update twitter.js
    Adding comments

--- twitter.js
+++ twitter.js
@@ -1,6 +1,7 @@
 var twitter = require('ntwitter');
-var io = require('socket.io').listen(1337);
+var io = require('socket.io').listen(1337); //WebSocket listening on port 1337
 
+//Open connection to twitter API using @vichargame account
 var twit = new twitter({
   consumer_key: 'WlX8QJIfRvo6HuIyW2QjZw',
   consumer_secret: 'imhvULLCCBonnBfV8Vv9bz0lZKMMMQo5QLgE2gsaI',
@@ -8,15 +9,17 @@ var twit = new twitter({
   access_token_secret: '8Iz4VcAlbd819rmUkqzMsbmXPM7AtcNPjTRjULZeEXc'
 });
 
+//Open WebSocket, on connection from client start streaming tweets which contain "#vichargame"
 io.sockets.on('connection', function(socket) {
   twit.stream('statuses/filter', {'track':'#vichargame'},
     function(stream) {
       stream.on('data',function(data){
-        socket.emit('twitter',data);
+        socket.emit('twitter',data); //Send tweet data to client when received
       });
     });
 });
 
+//Create server to serve out files in "Vi-Char-Splash" directory on port 80
 var connect = require('connect');
 connect.createServer(
     connect.static('Vi-Char-Splash')
