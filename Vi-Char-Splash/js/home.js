9c30d0bff8d5cae432f2d3126e8e42133034c473
webstuff <webstuff@puppetmaster.(none)>
2012-12-05 21:07:47 -0800

    Home page now displays the last 3 tweets which contain #vichargame. Also parses the created_at date the same way twitter does!

--- Vi-Char-Splash/js/home.js
+++ Vi-Char-Splash/js/home.js
@@ -1,3 +1,32 @@
+function parseTwitterDate(tdate) {
+    var system_date = new Date(Date.parse(tdate));
+    var user_date = new Date();
+    if (K.ie) {
+        system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
+    }
+    var diff = Math.floor((user_date - system_date) / 1000);
+    if (diff <= 1) {return "just now";}
+    if (diff < 20) {return diff + " seconds ago";}
+    if (diff < 40) {return "half a minute ago";}
+    if (diff < 60) {return "less than a minute ago";}
+    if (diff <= 90) {return "one minute ago";}
+    if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+    if (diff <= 5400) {return "1 hour ago";}
+    if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+    if (diff <= 129600) {return "1 day ago";}
+    if (diff < 604800) {return Math.round(diff / 86400) + " days ago";}
+    if (diff <= 777600) {return "1 week ago";}
+    return "on " + system_date;
+}
+
+// from http://widgets.twimg.com/j/1/widget.js
+var K = function () {
+    var a = navigator.userAgent;
+    return {
+        ie: a.match(/MSIE\s([^;]*)/)
+    }
+}();
+
 (function($) {
 	$(document).ready(function() {
 		//var $container = $('ul.tweets'),
@@ -8,7 +37,13 @@
 	    socket.on('twitter', function(data) {
 	        //$container.append(template.render(data));
 			//alert(data.text);
-			if($('.tweet').length == 3){
+			console.log(data.results.length);
+			var tweets = data.results;
+			for(var i=0;i<3;i++){
+				$('.tweets').append('<span class="tweet"><p>' + tweets[i].text + '</p><p class="tweet-attr">@' + tweets[i].from_user + ', ' + parseTwitterDate(tweets[i].created_at) + '</p></span>');
+				console.log(tweets[i]);
+			}
+			/*if($('.tweet').length == 3){
 				$('.tweet').first().slideUp().remove();
 				$('.tweets').append('<span class="tweet" style="display:none;"><p>' + data.text + '</p><p class="tweet-attr">@' + data.user.screen_name + ', ' + data.created_at + '</p></span>');
 				$('.tweets').find('.tweet:last').slideDown();
@@ -16,7 +51,7 @@
 			else{
 				$('.tweets').append('<span class="tweet" style="display:none;"><p>' + data.text + '</p><p class="tweet-attr">@' + data.user.screen_name + ', ' + data.created_at + '</p></span>');
 				$('.tweets').find('.tweet:last').slideDown();
-			}
+			}*/
 	    });
 	});
 })(jQuery);
