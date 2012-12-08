53920c673b8279cf641c30311d3fdff534b6c009
Scott Dunham <dunham.scott@gmail.com>
2012-12-08 12:50:19 -0800

    Adding python script to "start" the game

---
+++ tests/startGame.py
@@ -0,0 +1,17 @@
+import json, urllib2
+
+def changeState(data):
+    req = urllib2.Request('http://localhost:1730/gameState.json')
+    req.add_header('Content-Type', 'application/json')
+    response = urllib2.urlopen(req, json.dumps(data))
+    print json.load(response)
+
+def getState():
+    req = urllib2.Request('http://localhost:1730/gameState.json')
+    req.add_header('Content-Type', 'application/json')
+    response = urllib2.urlopen(req)
+    print json.load(response)
+	
+if __name__=="__main__":
+    data = {engine:{gameRunning:true}}
+    changeState(data)
